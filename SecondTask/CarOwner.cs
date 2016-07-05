59527b36568537aaabb8f2d73950a4e1755eb353
petrChe <petrchebanenko28@mail.ru>
2016-07-06 00:16:26 +0600

    almost completed an additional task

---
+++ SecondTask/CarOwner.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SecondTask
+{
+    class CarOwner
+    {
+        private string name;
+        private string brand;
+        private string carNumber;
+
+        public CarOwner(string name, string brand, string carNumber) {
+            this.name = name;
+            this.brand = brand;
+            this.carNumber = carNumber;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetBrand()
+        {
+            return this.brand;
+        }
+
+        public string GetCarNumber()
+        {
+            return this.carNumber;
+        }
+
+        public void SetName(string value)
+        {
+            this.name = value;
+        }
+
+        public void SetBrand(string value)
+        {
+            this.brand = value;
+        }
+
+        public void SetCarNumber(string value)
+        {
+            this.carNumber = value;
+        }
+
+        public bool nameValidation(CarOwner carOwner)
+        {
+            Regex nameRegex = new Regex(@"[A-Za-z]{3,15}");
+            if (nameRegex.IsMatch(carOwner.GetName()))
+            {
+                Console.WriteLine("Name is correct");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Name is incorrect");
+                return false;
+            }
+        }
+
+        public bool brandValidation(CarOwner carOwner)
+        {
+            Regex nameRegex = new Regex(@"[A-Za-z]{3,15}");
+            if (nameRegex.IsMatch(carOwner.GetBrand()))
+            {
+                Console.WriteLine("Brand is correct");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Brand is incorrect");
+                return false;
+            }
+        }
+
+        public bool carNumberValidation(CarOwner carOwner)
+        {
+            Regex nameRegex = new Regex(@"KZ[0-9]{3}[A-Z]{3}\d{2}");
+            if (nameRegex.IsMatch(carOwner.GetCarNumber()))
+            {
+                Console.WriteLine("Car number is correct");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Car number is incorrect");
+                return false;
+            }
+        }
+        
+        public  static List<CarOwner> GetInfoFromFile(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            List<CarOwner> carOwnersList = new List<CarOwner>();
+            
+            for(int i =0; i < lines.Length; i++)
+            {
+                string[] datas = new string[lines.Length];
+                datas = lines[i].Split(' ');
+                CarOwner carOwner = new CarOwner(datas[0], datas[1], datas[2]);
+                carOwnersList.Add(carOwner);
+                    
+                    
+                }
+                
+            
+            //foreach(CarOwner owner in carOwnersList)
+            //    {
+            //    Console.WriteLine(owner.GetCarNumber());
+            //}
+            return carOwnersList;
+        }
+
+        public static List<string> GetInfoByBrand(string brand)
+        {
+            List<CarOwner> carOwnersList = CarOwner.GetInfoFromFile(@"K:\Epam C Sharp dot net\textFile2.txt");
+            List<string> dataList = new List<string>();
+            foreach (CarOwner carOwner in carOwnersList)
+            {
+                if (carOwner.GetBrand().Equals(brand)) {
+                    string data = carOwner.GetName() + " " + carOwner.GetCarNumber();
+                    dataList.Add(data);
+                }
+            }
+            
+
+            return dataList;
+        }
+
+    }
+}
--- SecondTask/Program.cs
+++ SecondTask/Program.cs
@@ -12,118 +12,131 @@ namespace SecondTask
     {
         static void Main(string[] args)
         {
-            const double ACCURACY = 0.0001;// result depends on the accuracy
-            Console.Clear();
-            Console.WriteLine("White the n: ");
-            double n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("White the num: ");
-            double num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Newton method result " + NewtoneMethod(num, n, ACCURACY));
-            double powMethodResult = Math.Pow(num, 1 / n);
-            Console.WriteLine("Pow Method result " + powMethodResult);
-            Console.WriteLine("Enter number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Binary: " + DecimalToBinary(number));
-            Random random = new Random();
-            int rows = Convert.ToInt32(Console.ReadLine());
-            int columns = Convert.ToInt32(Console.ReadLine());
-            int[] firstMatrixArray = new int[rows];
-            int[,] secondMatrixArray = new int[rows, columns];
-            bool flag;
-            for (int i = 0; i < rows; i++)
-            {
-                firstMatrixArray[i] = random.Next(-10, 10);
-                for (int j = 0; j < columns; j++)
-                {
-                    secondMatrixArray[i, j] = random.Next(-10, 10);
-                }
-            }
-            string str2 = "";
-            for (int i = 0; i < rows; i++)
-            {
-                string str = "";
-                str2 += " " + firstMatrixArray[i].ToString() + " ";
-                for (int j = 0; j < columns; j++)
-                {
-                    str += secondMatrixArray[i, j].ToString() + " ";
-                }
+            //const double ACCURACY = 0.0001;// result depends on the accuracy
+            //Console.Clear();
+            //Console.WriteLine("White the n: ");
+            //double n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("White the num: ");
+            //double num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Newton method result " + NewtoneMethod(num, n, ACCURACY));
+            //double powMethodResult = Math.Pow(num, 1 / n);
+            //Console.WriteLine("Pow Method result " + powMethodResult);
+            //Console.WriteLine("Enter number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Binary: " + DecimalToBinary(number));
+            //Random random = new Random();
+            //int rows = Convert.ToInt32(Console.ReadLine());
+            //int columns = Convert.ToInt32(Console.ReadLine());
+            //int[] firstMatrixArray = new int[rows];
+            //int[,] secondMatrixArray = new int[rows, columns];
+            //bool flag;
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    firstMatrixArray[i] = random.Next(-10, 10);
+            //    for (int j = 0; j < columns; j++)
+            //    {
+            //        secondMatrixArray[i, j] = random.Next(-10, 10);
+            //    }
+            //}
+            //string str2 = "";
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    string str = "";
+            //    str2 += " " + firstMatrixArray[i].ToString() + " ";
+            //    for (int j = 0; j < columns; j++)
+            //    {
+            //        str += secondMatrixArray[i, j].ToString() + " ";
+            //    }
 
 
-                Console.WriteLine(str);
-            }
-            Console.WriteLine();
-            Console.WriteLine(str2);
-            Console.WriteLine();
-            for (int i = 0; i < rows; i++)
-            {
-                if (firstMatrixArray[i] < 0)
-                {
-                    flag = true;
-                }
-                else
-                {
-                    flag = false;
-                }
-                string str3 = "";
-                for (int j = 0; j < columns; j++)
-                {
-                    if (flag == true)
-                    {
-                        if (secondMatrixArray[i, j] < 0)
-                        {
-                            secondMatrixArray[i, j] = -1;
-                        }
-                        else if (secondMatrixArray[i, j] > 0)
-                        {
-                            secondMatrixArray[i, j] = 1;
-                        }
-                    }
-                    str3 += secondMatrixArray[i, j].ToString() + " ";
-                }
-                Console.WriteLine(str3);
-            }
+            //    Console.WriteLine(str);
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine(str2);
+            //Console.WriteLine();
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    if (firstMatrixArray[i] < 0)
+            //    {
+            //        flag = true;
+            //    }
+            //    else
+            //    {
+            //        flag = false;
+            //    }
+            //    string str3 = "";
+            //    for (int j = 0; j < columns; j++)
+            //    {
+            //        if (flag == true)
+            //        {
+            //            if (secondMatrixArray[i, j] < 0)
+            //            {
+            //                secondMatrixArray[i, j] = -1;
+            //            }
+            //            else if (secondMatrixArray[i, j] > 0)
+            //            {
+            //                secondMatrixArray[i, j] = 1;
+            //            }
+            //        }
+            //        str3 += secondMatrixArray[i, j].ToString() + " ";
+            //    }
+            //    Console.WriteLine(str3);
+            //}
 
-            Console.WriteLine("Write the number of rows:");
-            int rowCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Write the number of columns:");
-            int columnCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The matrix:");
-            Matrix matrix = CreateMatrix(rowCount, columnCount);
-            matrix.MatrixToString(matrix);
-            Console.WriteLine();
-            Console.WriteLine("The inverted matrix:");
-            matrix.MatrixToString(matrix.invertMatrix(matrix));
+            //Console.WriteLine("Write the number of rows:");
+            //int rowCount = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Write the number of columns:");
+            //int columnCount = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("The matrix:");
+            //Matrix matrix = CreateMatrix(rowCount, columnCount);
+            //matrix.MatrixToString(matrix);
+            //Console.WriteLine();
+            //Console.WriteLine("The inverted matrix:");
+            //matrix.MatrixToString(matrix.invertMatrix(matrix));
+
+            //Console.WriteLine("Write the number of rows:");
+            //int firstRowCount = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Write the number of columns:");
+            //int firstColumnCount = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("The first matrix:");
+            //Matrix firstMatrix = CreateMatrix(firstRowCount, firstColumnCount);
+            //firstMatrix.MatrixToString(firstMatrix);
+            //Console.WriteLine("Write the number of rows:");
+            //int secondRowCount = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Write the number of columns:");
+            //int secondColumnCount = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("The second matrix:");
+            //Matrix secondMatrix = CreateMatrix(secondRowCount, secondColumnCount);
+            //secondMatrix.MatrixToString(secondMatrix);
+
+            //MatrixOperator firstMatrixOperator = new MatrixOperator();
+            //Matrix addResultMatrix = firstMatrixOperator.MatrixAddition(firstMatrix, secondMatrix);
+            //Console.WriteLine("The result of add matrix:");
+            //addResultMatrix.MatrixToString(addResultMatrix);
+
+            //MatrixOperator secondMatrixOperator = new MatrixOperator();
+            //Matrix subResultMatrix = secondMatrixOperator.MatrixSubtraction(firstMatrix, secondMatrix);
+            //Console.WriteLine("The result of sub matrix:");
+            //subResultMatrix.MatrixToString(subResultMatrix);
+
+            //MatrixOperator thirdMatrixOperator = new MatrixOperator();
+            //Matrix multiplyResultMatrix = thirdMatrixOperator.MatrixMultiply(firstMatrix, secondMatrix);
+            //Console.WriteLine("The result of multiply matrix:");
+            //multiplyResultMatrix.MatrixToString(multiplyResultMatrix);
+            CarOwner carOwner = new CarOwner("fedotov", "bmw", "KZ222EPA02");
+            if(carOwner.carNumberValidation(carOwner))
+            {
+                Console.WriteLine("Regulars are working!");
+            }
+            else
+            {
+                Console.WriteLine("Regulars are not  working!");
+            }
             
-            Console.WriteLine("Write the number of rows:");
-            int firstRowCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Write the number of columns:");
-            int firstColumnCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The first matrix:");
-            Matrix firstMatrix = CreateMatrix(firstRowCount, firstColumnCount);
-            firstMatrix.MatrixToString(firstMatrix);
-            Console.WriteLine("Write the number of rows:");
-            int secondRowCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Write the number of columns:");
-            int secondColumnCount = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The second matrix:");
-            Matrix secondMatrix = CreateMatrix(secondRowCount, secondColumnCount);
-            secondMatrix.MatrixToString(secondMatrix);
-
-            MatrixOperator firstMatrixOperator = new MatrixOperator();
-            Matrix addResultMatrix = firstMatrixOperator.MatrixAddition(firstMatrix, secondMatrix);
-            Console.WriteLine("The result of add matrix:");
-            addResultMatrix.MatrixToString(addResultMatrix);
-
-            MatrixOperator secondMatrixOperator = new MatrixOperator();
-            Matrix subResultMatrix = secondMatrixOperator.MatrixSubtraction(firstMatrix, secondMatrix);
-            Console.WriteLine("The result of sub matrix:");
-            subResultMatrix.MatrixToString(subResultMatrix);
-
-            MatrixOperator thirdMatrixOperator = new MatrixOperator();
-            Matrix multiplyResultMatrix = thirdMatrixOperator.MatrixMultiply(firstMatrix, secondMatrix);
-            Console.WriteLine("The result of multiply matrix:");
-            multiplyResultMatrix.MatrixToString(multiplyResultMatrix);
-            
+            foreach(string data in CarOwner.GetInfoByBrand("Nissan"))
+            {
+                Console.WriteLine(data);
+            }
             Console.ReadKey();
         }
     }
